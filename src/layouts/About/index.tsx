eadbd684216c807f013ba8928a7ab049487e5d32
brunoesposito <brunoesposito@MacBook-de-Bruno.local>
2021-03-30 16:02:43 -0300

    google verificated

--- src/layouts/About/index.tsx
+++ src/layouts/About/index.tsx
@@ -2,31 +2,33 @@ import React, { FC } from 'react';
 import Image from 'next/image';
 
 import { Title, Button } from 'components';
-import { Container, Content, Text, BoxImage, Link } from './styles';
+import { Container, Content, Text, BoxImage } from './styles';
 
-const About: FC = () => (
-  <Container id="about-jussi">
-    <div className="container d-flex justify-between">
-      <Content>
-        <Title>OLÁ, SOMOS A JÜSSI</Title>
-        <Text>
-          A Jüssi é uma agência integrante do grupo global WPP que vem há 10
-          anos consolidando o pensamento voltado para produtos e resolução de
-          problemas. Nosso área dedicada exclusivamente para Produtos Digitais é
-          organizada em 6 especialidades: Product Managamenet, User Experience
-          Design, SEO, Tecnologia, Agile e User Behavior Analytics.
-        </Text>
-        <Button color="black" outline>
-          Conheça a Jüssi
-        </Button>
-      </Content>
-      <BoxImage>
-        <Link href="http://jussi.com.br/" target="_blank">
+const About: FC = () => {
+  const openJussi = () => window.open('http://jussi.com.br/', '_blank');
+
+  return (
+    <Container id="about-jussi">
+      <div className="container d-flex justify-between">
+        <Content>
+          <Title>OLÁ, SOMOS A JÜSSI</Title>
+          <Text>
+            A Jüssi é uma agência integrante do grupo global WPP que vem há 10
+            anos consolidando o pensamento voltado para produtos e resolução de
+            problemas. Nosso área dedicada exclusivamente para Produtos Digitais
+            é organizada em 6 especialidades: Product Managamenet, User
+            Experience Design, SEO, Tecnologia, Agile e User Behavior Analytics.
+          </Text>
+          <Button onClick={openJussi} color="black" outline>
+            Conheça a Jüssi
+          </Button>
+        </Content>
+        <BoxImage>
           <Image src="/jussi.png" width={676} height={520} />
-        </Link>
-      </BoxImage>
-    </div>
-  </Container>
-);
+        </BoxImage>
+      </div>
+    </Container>
+  );
+};
 
 export default About;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,10 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
       <meta charSet="UTF-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#ffffff" />
+      <meta
+        name="google-site-verification"
+        content="Grzq8h_Gq7-8yfhfbwnpakbX0Mw2Hu1tiCJfeYvQYko"
+      />
     </Head>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
